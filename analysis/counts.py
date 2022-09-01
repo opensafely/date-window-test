89c21c3cc8f252be3f9002614f9775b9f88a871e
Alex Walker <alex.walker@thedatalab.org>
2022-09-01 14:41:23 +0100

    add percentage to output tables

--- analysis/counts.py
+++ analysis/counts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-for day in range(3,10):
+for day in range(3, 10):
     before_fix = pd.read_csv(f"output/before_fix/input_2020-02-0{day}.csv")
     after_fix = pd.read_csv(f"output/after_fix/input_2020-02-0{day}.csv")
 
@@ -8,7 +8,8 @@ for day in range(3,10):
     after_sum = after_fix.sum()
 
     delta = after_sum - before_sum
+    percent_change = (delta / before_sum) * 100
 
-    combined = pd.concat([before_sum, after_sum, delta], axis=1)
+    combined = pd.concat([before_sum, after_sum, delta, percent_change], axis=1)
     print(combined)
     combined.to_csv(f"output/counts_2020-02-0{day}.csv")
