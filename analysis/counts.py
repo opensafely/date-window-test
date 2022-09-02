57aa1702a6de4b669d560cedee595948499e034f
Alex Walker <alex.walker@thedatalab.org>
2022-09-02 11:40:32 +0100

    redact small counts

--- analysis/counts.py
+++ analysis/counts.py
@@ -1,8 +1,18 @@
+import numpy as np
 import pandas as pd
+
+
+def redact_small_numbers(df, column):
+    mask = df[column].isin([1, 2, 3, 4, 5])
+    df.loc[mask, :] = np.nan
+    return df
+
 
 for day in range(3, 10):
     before_fix = pd.read_csv(f"output/before_fix/input_2020-02-0{day}.csv")
     after_fix = pd.read_csv(f"output/after_fix/input_2020-02-0{day}.csv")
+    before_fix = before_fix.drop(columns=["patient_id"])
+    after_fix = after_fix.drop(columns=["patient_id"])
 
     before_sum = before_fix.sum()
     after_sum = after_fix.sum()
@@ -12,4 +22,4 @@ for day in range(3, 10):
 
     combined = pd.concat([before_sum, after_sum, delta, percent_change], axis=1)
     print(combined)
-    combined.to_csv(f"output/counts_2020-02-0{day}.csv")
+    redact_small_numbers(combined, "delta").to_csv(f"output/counts_2020-02-0{day}.csv")
